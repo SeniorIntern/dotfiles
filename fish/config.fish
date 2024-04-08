72dce7992bd3e8daa1edb7b1d9935d65bd1e24a2
SeniorIntern <tnikhil.official@gmail.com>
2024-04-08 20:07:08 +0545

    Stable dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
---
+++ nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,7 @@
+return {
+	'echasnovski/mini.pairs',
+	branch = 'stable',
+	config = function()
+		require('mini.pairs').setup()
+	end
+}
