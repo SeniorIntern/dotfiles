8e1b63100737c43e29103bf7571361cc48811131
SeniorIntern <tnikhil.official@gmail.com>
2026-02-22 18:30:42 +0400

    Saving my stable config

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("keymaps")
+require("options")
+require("lazy_init")
---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				vue = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				csss = { "prettier" },
+				python = { "isort", "black" },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cf", function()
+			require("conform").format({ bufnr = 0 })
+		end)
+	end,
+}
