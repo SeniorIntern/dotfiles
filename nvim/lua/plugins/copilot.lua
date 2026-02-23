664074aee8a206be023ac0dc7b01497b3b67ed64
SeniorIntern <tnikhil.official@gmail.com>
2026-02-23 20:37:40 +0400

    ADd copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	opts = {
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+		},
+	},
+	config = function(_, opts)
+		require("copilot").setup(opts)
+
+		-- disable by default
+		vim.cmd("Copilot disable")
+	end,
+}
