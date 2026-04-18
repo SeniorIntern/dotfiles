755435ebada9fbd5158350807fb3801deb519ac6
SeniorIntern <tnikhil.official@gmail.com>
2026-04-18 16:00:45 +0400

    Setup onedark pro theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,47 +1,7 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			-- vim.cmd([[colorscheme tokyonight-moon]])
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = false,
-				styles = {
-					italic = false,
-				},
-			})
-			-- vim.cmd.colorscheme("rose-pine-moon")
-		end,
-	},
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000,
-		config = function()
-			-- vim.cmd("colorscheme onedark")
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_better_performance = "1"
-			vim.g.gruvbox_material_foreground = "material" -- Available values:   `'material'`, `'mix'`, `'original'`
-			vim.g.gruvbox_material_background = "soft" -- Available values:   `'hard'`, `'medium'`, `'soft'`
-			vim.g.gruvbox_material_ui_contrast = "low" -- Available values:   `'low'`, `'high'`
-			vim.g.gruvbox_material_float_style = "dim"
-			vim.g.gruvbox_material_statusline_style = "mix"
-			vim.g.gruvbox_material_cursor = "auto"
-			vim.g.gruvbox_material_dim_inactive_windows = 1
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
+	"olimorris/onedarkpro.nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme onedark")
+	end,
 }
